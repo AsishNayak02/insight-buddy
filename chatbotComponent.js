4039f29b0cce63a4cb03a58a3636c188e0a78745
divyam <divyam.prakash@eximietas.com>
2024-01-19 10:47:42 +0530

    feat: add notification
    Inroduce dot to the notification.

--- chatbotComponent.js
+++ chatbotComponent.js
@@ -1,5 +1,4 @@
 const styles = `
-
 .card-container {
     
   height: 50vh;
@@ -211,6 +210,15 @@ margin-right: 400px;
   z-index: 9;
 }
 
+.red-dot {
+  width: 10px;
+  height: 10px;
+  background-color: red;
+  border-radius: 50%;
+  position: absolute;
+  top: 5px;
+  right: 5px;
+}
 /* .form-container {
   max-width: 300px;
   padding: 10px;
@@ -218,203 +226,221 @@ margin-right: 400px;
   margin-bottom: 70px;
 }
  */`
-
-
-const styleElement = document.createElement('style');
-styleElement.innerHTML = styles;
-document.head.appendChild(styleElement);
-
+ const styleElement = document.createElement('style');
+ styleElement.innerHTML = styles;
+ document.head.appendChild(styleElement);
  let unreadMessagesCount = 0;
  let btnClose = 0;
  const audioElement = document.createElement('audio');
   audioElement.src = 'ping-82822.mp3';
-
-function openForm() {
-  btnClose = 0;
-  unreadMessagesCount = 0;
-  document.getElementById("myForm").style.display = "block";
+ function openForm() {
+    btnClose=0;
+   document.getElementById("myForm").style.display = "block";
+  let unreadMessagesCount = 0;
   setTimeout(() => {
     const inputField = document.querySelector('.user-input');
     if (inputField) {
       inputField.focus();
     }
   }, 0);
-}
-function closeForm() {
+   updatePopupIcon();
+ }
+ function closeForm() {
   btnClose = 1;
   document.getElementById("myForm").style.display = "none";
+  updateCloseButtonIcon();
 }
-
-function createChatbox(btncr) {
-  const chatboxContent = `
-    <div class="chat-popup" id="myForm">
-      <div class="form-container" id="chatbot-container" ></div>
-      <button type="button" class="close-button" onclick="closeForm()" style="background-color: ${btncr};">🤖</button>
-    </div>
-    <button class="open-button" onclick="openForm()" style="background-color: ${btncr};">🤖</button>
-  `;
-  document.body.innerHTML += chatboxContent;
-}
-const ChatbotComponent = (apiEndpoint, btncr, title,avt) => {
-  const state = {
-    userMessage: '',
-    chatHistory: [],
-    products: [],
-  };
-  const addWelcomeMessage = () => {
-    const welcomeMessage = { text: 'Welcome! How can I assist you today?', sender: 'bot' };
-    state.chatHistory = [welcomeMessage];
-    render();
-  };
-  const welcomeMessageTimeout = setTimeout(addWelcomeMessage, 1000);
-  const handleUserMessageChange = (e) => {
-    state.userMessage = e.target.value;
-  };
-  const handleKeyPress = (e) => {
-    if (e.key === 'Enter') {
-      handleSendMessage();
-    }
-  };
-
-  const handleBeep = (message) => {
+ function updatePopupIcon() {
+   const popupIcon = document.querySelector('.open-button');
+   const notificationSymbol = document.querySelector('.notification-symbol');
+   if (unreadMessagesCount > 0) {
+     notificationSymbol.textContent = unreadMessagesCount;
+     notificationSymbol.classList.add('active');
+   } else {
+     notificationSymbol.classList.remove('active');
+   }
+   popupIcon.textContent = '🤖';
+   
+ }
+ function updateCloseButtonIcon() {
+   const closeButton = document.querySelector('.close-button');
+   const notificationSymbol = document.querySelector('.notification-symbol-close');
+   if (unreadMessagesCount > 0 ) {
+     notificationSymbol.textContent = unreadMessagesCount;
+     notificationSymbol.classList.add('active');
+   } else {
+     notificationSymbol.classList.remove('active');
+   }
+   closeButton.textContent = '🤖';
+ }
+ function createChatbox(btncr) {
+   const chatboxContent = `
+     <div class="chat-popup" id="myForm">
+       <div class="form-container" id="chatbot-container" ></div>
+       <button type="button" class="close-button" onclick="closeForm()" style="background-color: ${btncr};">🤖<span class="notification-symbol-close"></span></button>
+     </div>
+     <button class="open-button" onclick="openForm()" style="background-color: ${btncr};">🤖<span class="notification-symbol"></span></button>
+   `;
+   document.body.innerHTML += chatboxContent;
+ }
+ const ChatbotComponent = (apiEndpoint, btncr, title, avt) => {
+   const state = {
+     userMessage: '',
+     chatHistory: [],
+     products: [],
+   };
+   const addWelcomeMessage = () => {
+     const welcomeMessage = { text: 'Welcome! How can I assist you today?', sender: 'bot' };
+     state.chatHistory = [welcomeMessage];
+     render();
+   };
+   const welcomeMessageTimeout = setTimeout(addWelcomeMessage, 1000);
+   const handleUserMessageChange = (e) => {
+     state.userMessage = e.target.value;
+   };
+   const handleKeyPress = (e) => {
+     if (e.key === 'Enter') {
+       handleSendMessage();
+     }
+   };
+   const handleBeep = (message) => {
     console.log("handle beep called");
     if (message.sender === 'bot' && message.text !== 'typing...' && btnClose === 1) {
       audioElement.play();
       unreadMessagesCount++;
+      addRedDotToButton('.open-button');
+      addRedDotToButton('.close-button');
     }
    }
-
-  const handleSendMessage = () => {
-    const fetchProducts = async () => {
-      try {
-        const response = await fetch(apiEndpoint);
-        const data = await response.json();
-        state.products = data;
-        render();
-      } catch (error) {
-        console.error('Error fetching products:', error);
-      }
-    };
-    fetchProducts();
-    const userM = state.userMessage;
-    state.userMessage='';
-    if (!userM.trim()) return;
-    const selectedAvatar = avt;
-    const newUserMessageWithAvatar = {
-      text: userM,
-      sender: 'user',
-      avatar: selectedAvatar,
-    };
-    state.chatHistory = [...state.chatHistory, newUserMessageWithAvatar];
-    const botTyping = { text: 'typing...', sender: 'bot' };
-    state.chatHistory = [...state.chatHistory, botTyping];
-    const sanitizedUserMessage = userM.toLowerCase().replace(/\s/g, '');
-    setTimeout(()=>{
-    if (sanitizedUserMessage === 'hi' || sanitizedUserMessage === 'hello') {
-      const botResponse = { text: 'Hello! How can I assist you today?', sender: 'bot' };
-      state.chatHistory = [...state.chatHistory, botResponse];
-      handleBeep(botResponse);
-    } else if (sanitizedUserMessage === 'whoareyou') {
-      const botResponse = { text: "I'm a friendly chatbot here to help!", sender: 'bot' };
-      state.chatHistory = [...state.chatHistory, botResponse];
-      handleBeep(botResponse);
-    } else {
-      let foundProduct = null;
-      for (let i = 0; i < state.products.length; i++) {
-        const sanitizedTitle = state.products[i].title.toLowerCase().replace(/\s/g, '');
-        if (sanitizedTitle.includes(sanitizedUserMessage)) {
-          foundProduct = state.products[i];
-          break;
-        }
-      }
-      if (foundProduct) {
-        const { id, title, price } = foundProduct;
-        const botResponse = { text: `Price for ${title}: $${price}`, sender: 'bot' };
-        state.chatHistory = [...state.chatHistory, botResponse];
-        handleBeep(botResponse);
-      } else {
-        const botResponse = { text: `Product "${userM}" not found.`, sender: 'bot' };
-        state.chatHistory = [...state.chatHistory, botResponse];
-        handleBeep(botResponse);
-      }
-    }
+   function addRedDotToButton(buttonSelector) {
+    const button = document.querySelector(buttonSelector);
+    const redDot = document.createElement('div');
+    redDot.className = 'red-dot';
+    button.appendChild(redDot);
     
-    state.chatHistory = state.chatHistory.filter((message) => message.sender !== 'bot' || message.text !== 'typing...');
-    render();
-  },5000)
-  };
-  const render = () => {
-    document.body.onload = function() {createChatbox(btncr);};
-    const chatbotContainer = document.getElementById('chatbot-container');
-    chatbotContainer.innerHTML = '';
-
-    const cardContainer = document.createElement('div');
-    cardContainer.className = 'card-container';
-
-    const card = document.createElement('div');
-    card.className = 'card';
-
-    const titleBar = document.createElement('div');
-    titleBar.className = 'title-bar';
-    titleBar.style.backgroundColor = btncr;
-
-    const icon = document.createElement('span');
-    icon.className = 'icon';
-    icon.textContent = '🤖';
-
-    const h4 = document.createElement('h4');
-    h4.style.color = 'white';
-    h4.textContent = title;
-
-    titleBar.appendChild(icon);
-    titleBar.appendChild(h4);
-    card.appendChild(titleBar);
-
-    const chatHistoryContainer = document.createElement('div');
-    chatHistoryContainer.className = 'chat-history-container';
-
-    state.chatHistory.forEach((message, index) => {
-      const messageDiv = document.createElement('div');
-      messageDiv.className = `chat-message ${message.sender === 'user' ? 'user-message' : 'bot-message'}`;
-      messageDiv.textContent = message.text;
-      if (message.sender === 'user') {
-        const avatarSpan = document.createElement('span');
-        avatarSpan.className = 'avatar';
-        avatarSpan.textContent = message.avatar;
-        chatHistoryContainer.appendChild(avatarSpan);
-      }
-      chatHistoryContainer.appendChild(messageDiv);
-    });
-
-    card.appendChild(chatHistoryContainer);
-
-    const inputContainer = document.createElement('div');
-    inputContainer.className = 'input-container';
-
-    const input = document.createElement('input');
-    input.className = 'user-input';
-    input.type = 'text';
-    input.value = state.userMessage;
-    input.placeholder = 'Enter Request...';
-    input.addEventListener('input', handleUserMessageChange);
-    input.addEventListener("keyup", handleKeyPress);
-
-    const button = document.createElement('button');
-    button.className = 'send-btn';
-    button.textContent = 'Send';
-    button.style.backgroundColor = btncr;
-    button.addEventListener('click', handleSendMessage);
-
-    inputContainer.appendChild(input);
-    inputContainer.appendChild(button);
-    card.appendChild(inputContainer);
-
-    cardContainer.appendChild(card);
-    chatbotContainer.appendChild(cardContainer);
-    chatHistoryContainer.scrollTop = chatHistoryContainer.scrollHeight;
   }
+   const handleSendMessage = () => {
+    console.log("Send message");
+     const fetchProducts = async () => {
+       try {
+         const response = await fetch(apiEndpoint);
+         const data = await response.json();
+         state.products = data;
+         render();
+       } catch (error) {
+         console.error('Error fetching products:', error);
+       }
+     };
+     fetchProducts();
+     const userM = state.userMessage;
+     state.userMessage = '';
+     if (!userM.trim()) return;
+     const selectedAvatar = avt;
+     const newUserMessageWithAvatar = {
+       text: userM,
+       sender: 'user',
+       avatar: selectedAvatar,
+     };
+     state.chatHistory = [...state.chatHistory, newUserMessageWithAvatar];
+     const botTyping = { text: 'typing...', sender: 'bot' };
+     state.chatHistory = [...state.chatHistory, botTyping];
+     const sanitizedUserMessage = userM.toLowerCase().replace(/\s/g, '');
+     setTimeout(() => {
+       if (sanitizedUserMessage === 'hi' || sanitizedUserMessage === 'hello') {
+         const botResponse = { text: 'Hello! How can I assist you today?', sender: 'bot' };
+         state.chatHistory = [...state.chatHistory, botResponse];
+         handleBeep(botResponse);
+       } else if (sanitizedUserMessage === 'whoareyou') {
+         const botResponse = { text: "I'm a friendly chatbot here to help!", sender: 'bot' };
+         state.chatHistory = [...state.chatHistory, botResponse];
+         handleBeep(botResponse);
+       } else {
+         let foundProduct = null;
+         for (let i = 0; i < state.products.length; i++) {
+           const sanitizedTitle = state.products[i].title.toLowerCase().replace(/\s/g, '');
+           if (sanitizedTitle.includes(sanitizedUserMessage)) {
+             foundProduct = state.products[i];
+             break;
+           }
+         }
+         if (foundProduct) {
+           const { id, title, price } = foundProduct;
+           const botResponse = { text: `Price for ${title}: $${price}`, sender: 'bot' };
+           state.chatHistory = [...state.chatHistory, botResponse];
+           handleBeep(botResponse);
+         } else {
+           const botResponse = { text: `Product "${userM}" not found.`, sender: 'bot' };
+           state.chatHistory = [...state.chatHistory, botResponse];
+           handleBeep(botResponse);
+         }
+       }
+       state.chatHistory = state.chatHistory.filter((message) => message.sender !== 'bot' || message.text !== 'typing...');
+       render();
+     }, 5000);
+   };
+   const render = () => {
+     document.body.onload = function () { createChatbox(btncr); };
+     const chatbotContainer = document.getElementById('chatbot-container');
+     chatbotContainer.innerHTML = '';
+     const cardContainer = document.createElement('div');
+     cardContainer.className = 'card-container';
+     const card = document.createElement('div');
+     card.className = 'card';
+     const titleBar = document.createElement('div');
+     titleBar.className = 'title-bar';
+     titleBar.style.backgroundColor = btncr;
+     const icon = document.createElement('span');
+     icon.className = 'icon';
+     icon.textContent = '🤖';
+     const h4 = document.createElement('h4');
+     h4.style.color = 'white';
+     h4.textContent = title;
+     titleBar.appendChild(icon);
+     titleBar.appendChild(h4);
+     card.appendChild(titleBar);
+     const chatHistoryContainer = document.createElement('div');
+     chatHistoryContainer.className = 'chat-history-container';
+     state.chatHistory.forEach((message, index) => {
+       const messageDiv = document.createElement('div');
+       messageDiv.className = `chat-message ${message.sender === 'user' ? 'user-message' : 'bot-message'}`;
+       messageDiv.textContent = message.text;
+       if (message.sender === 'user') {
+         const avatarSpan = document.createElement('span');
+         avatarSpan.className = 'avatar';
+         avatarSpan.textContent = message.avatar;
+         chatHistoryContainer.appendChild(avatarSpan);
+       }
+       chatHistoryContainer.appendChild(messageDiv);
+     });
+     card.appendChild(chatHistoryContainer);
+     const inputContainer = document.createElement('div');
+     inputContainer.className = 'input-container';
+     const input = document.createElement('input');
+     input.className = 'user-input';
+     input.type = 'text';
+     input.value = state.userMessage;
+     input.placeholder = 'Enter Request...';
+     input.addEventListener('input', handleUserMessageChange);
+     input.addEventListener("keyup", handleKeyPress);
+     const button = document.createElement('button');
+     button.className = 'send-btn';
+     button.textContent = 'Send';
+     button.style.backgroundColor = btncr;
+     button.addEventListener('click', handleSendMessage);
+     inputContainer.appendChild(input);
+     inputContainer.appendChild(button);
+     card.appendChild(inputContainer);
+     cardContainer.appendChild(card);
+     chatbotContainer.appendChild(cardContainer);
+     chatHistoryContainer.scrollTop = chatHistoryContainer.scrollHeight;
+   }
+   return {
+     render,
+   };
+ };
 
-  return {
-    render,
-  };
-};
+
+
+
+
+
